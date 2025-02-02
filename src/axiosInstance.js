9b2c48ba37b71853e5db2d8a987f2a81e14d9f80
Umeshkumar52 <uk3993907@gmail.com>
2025-02-02 11:42:09 +0530

    update

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-const url="http://localhost:5000"
-//  const url="https://beyondchatserver.onrender.com/send-otp"
+// const url="http://localhost:5000"
+ const url="https://beyondchatserver.onrender.com"
  const axiosInstance= axios.create({
     baseURL:url,
     // 'withCredentials':true,
