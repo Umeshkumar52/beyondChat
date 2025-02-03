fa50186cf199ae9799e9137373861de661b8c584
Umeshkumar52 <123877527+Umeshkumar52@users.noreply.github.com>
2025-02-03 12:43:22 +0530

    Update GoogleAuth.jsx

--- src/components/GoogleAuth.jsx
+++ src/components/GoogleAuth.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
 import { jwtDecode } from 'jwt-decode';
 import { useNavigate } from 'react-router-dom';
-const clientId = '708464769189-pi4nejq40haojf53kg3g1da9lenqb5a9.apps.googleusercontent.com';
+const clientId = '296055646871-h7t8vokik6beui0v8rc7rqarujaegbs4.apps.googleusercontent.com';
 export default function GoogleAuth() {
   const navigate=useNavigate()
   return (
