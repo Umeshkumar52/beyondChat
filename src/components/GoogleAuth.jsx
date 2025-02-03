37d1b60e351e626541cee87ea2ac0099248ada50
Umeshkumar52 <123877527+Umeshkumar52@users.noreply.github.com>
2025-02-03 12:37:11 +0530

    Update GoogleAuth.jsx

--- src/components/GoogleAuth.jsx
+++ src/components/GoogleAuth.jsx
@@ -2,19 +2,18 @@ import React from 'react'
 import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
 import { jwtDecode } from 'jwt-decode';
 import { useNavigate } from 'react-router-dom';
-const clientId = '708464769189-2o2ek5nrmfpn2clb3r08m38ajsncraqe.apps.googleusercontent.com';
+const clientId = '708464769189-pi4nejq40haojf53kg3g1da9lenqb5a9.apps.googleusercontent.com';
 export default function GoogleAuth() {
   const navigate=useNavigate()
   return (
     <GoogleOAuthProvider clientId={clientId}>
     <GoogleLogin
       onSuccess={(credentialResponse) => {
-        const decoded = jwtDecode(credentialResponse.credential);
-        console.log('User Info:', decoded);
+        const decoded = jwtDecode(credentialResponse.credential)
         navigate('/')
       }}
       onError={() => {
-        return
+        console.log("Login Failed")
       }}
     />
   </GoogleOAuthProvider>
